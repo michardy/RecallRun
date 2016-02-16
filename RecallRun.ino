9d31b5219593e65c0ca41be41ddde91580b43e53
michardy <mhardy17@bayschoolsf.org>
2016-02-16 12:03:38 -0800

    Initial commit

---
+++ RecallRun.ino
@@ -0,0 +1,29 @@
+#include <EEPROM.h>
+#define btnPin 3
+
+bool mode = false;
+//int iter = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(btnPin,INPUT);
+  if (digitalRead(btnPin) == 0){
+    mode = true;
+  }
+  for (int i=0; i<512; i+=3){
+    Serial.println(EEPROM.read(i)+', '+EEPROM.read(i+1)+', '+EEPROM.read(i+2));
+  }
+}
+
+void loop() {
+  if (!(mode)){
+    //do things
+    /*int val = 4;
+    EEPROM.write(iter, val);
+    iter++
+    EEPROM.write(iter, val);
+    iter++
+    EEPROM.write(iter, val);
+    iter++*/
+    }
+}
